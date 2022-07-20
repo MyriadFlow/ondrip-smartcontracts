d5fcb60602f4746e510a0cd250e0e804c658d406
Jonathan Lourie <65506981+jonlourie@users.noreply.github.com>
2022-07-20 18:52:05 -0400

    Fixed errors on purchasing, added new event argument
    added an additional event and function for the removal of items from the marketplace

--- contracts/OnDripMarket.sol
+++ contracts/OnDripMarket.sol
@@ -1,34 +1,28 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/interfaces/IERC2981.sol";
-
-//INTERFACE
-import "./OnDripNFT.sol";
+import "@openzeppelin/contracts/token/common/ERC2981.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract OnDripMarket is ReentrancyGuard {
     using Counters for Counters.Counter;
     Counters.Counter private _itemIds;
     Counters.Counter private _itemsSold;
 
-    address public payoutAddress;
     uint96 public platformFeeBasisPoint;
-
-    OnDripNFT public onDripNFT;
-
-    address onDripNFTAddress;
+    address public owner;
      
     constructor(
-         address _onDripNFT,
+   
+         address _owner,
          uint96 _platformFee
+         
      ) {
 
-        onDripNFTAddress = _onDripNFT;
         platformFeeBasisPoint = _platformFee;
-        payoutAddress = msg.sender;
+        owner = _owner;
      }
      
      struct MarketItem {
@@ -38,73 +32,124 @@ contract OnDripMarket is ReentrancyGuard {
          address payable seller;
          address payable owner;
          uint256 price;
-         bool sold;
+         bool forSale;
+         bool deleted;
      }
      
-     mapping(uint256 => MarketItem) private idToMarketItem;
+     mapping(uint256 => MarketItem) public idToMarketItem;
      
      event MarketItemCreated (
         uint indexed itemId,
-        address indexed nftContract, //in case we have multiple NFT contracts
+        address indexed nftContract, 
         uint256 indexed tokenId,
+        string metaDataURI,
         address seller,
         address owner,
         uint256 price,
-        bool sold
+        bool forSale
      );
      
-     event MarketItemSold (
+    event MarketItemSold (
          uint indexed itemId,
          uint indexed tokenId,
          address buyer,
          uint256 price 
     );
-    
+
+    event MarketItemRemoved(uint256 itemId);
+
+    //Modifers 
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    modifier onlyWhenItemIsForSale(uint256 itemId){
+        require(
+            idToMarketItem[itemId].forSale == true,
+            "Marketplace: Market item is not for sale"
+        );
+        _;
+    }
+
+    modifier onlyItemOwner(address nftContract, uint256 tokenId) {
+        require(
+            IERC721(nftContract).ownerOf(tokenId) == msg.sender,
+            "Marketplace: Sender does not own the item"
+        );
+        _;
+    }
+
+    modifier onlySeller(uint256 itemId) {
+        require(
+            idToMarketItem[itemId].seller == msg.sender,
+            "Marketplace: Sender is not seller of this item"
+        );
+        _;
+    } 
+
     function createMarketItem(
-        //address nftContract,
+        address nftContract, //OPTIONAL IN CASE THERE ARE MULTIPLE SUB ITEMS
         uint256 tokenId,
         uint256 price
-        ) public payable nonReentrant {
+        ) public onlyItemOwner(nftContract, tokenId) returns (uint256) {
             require(price > 0, "Price must be greater than 0");
-            
+
             _itemIds.increment();
             uint256 itemId = _itemIds.current();
   
             idToMarketItem[itemId] =  MarketItem(
                 itemId,
-                onDripNFTAddress,
+                nftContract,
                 tokenId,
                 payable(msg.sender),
                 payable(address(0)),
                 price,
+                true,
                 false
             );
             
-            IERC721(onDripNFT).transferFrom(msg.sender, address(this), tokenId);
+            
+            IERC721(nftContract).transferFrom(msg.sender, address(this), tokenId);
+            
+            string memory metadataURI = IERC721Metadata(nftContract).tokenURI(tokenId);
                 
             emit MarketItemCreated(
                 itemId,
-                onDripNFTAddress,
+                nftContract,
                 tokenId,
+                metadataURI,
                 msg.sender,
                 address(0),
                 price,
                 false
             );
+
+            return itemId;
         }
-        
+
+    function removeFromSale(uint256 itemId) public onlySeller(itemId) {
+        IERC721(idToMarketItem[itemId].nftContract).transferFrom(
+            address(this),
+            idToMarketItem[itemId].seller,
+            idToMarketItem[itemId].tokenId
+        );
+        idToMarketItem[itemId].deleted = true;
+
+        emit MarketItemRemoved(itemId);
+        delete idToMarketItem[itemId];
+    }
+
     function createMarketSale(
-        //address nftContract,
         uint256 itemId
-        ) public payable nonReentrant {
+        ) public payable nonReentrant onlyWhenItemIsForSale(itemId) {
             uint price = idToMarketItem[itemId].price;
             uint tokenId = idToMarketItem[itemId].tokenId;
-            bool sold = idToMarketItem[itemId].sold;
-            require(msg.value == price, "Please submit the asking price in order to complete the purchase");
-            require(sold != true, "This Sale has alredy finnished");
+            require(msg.value >= price, "Please submit the asking price in order to complete the purchase");
 
             //Royalty
-            (address accountReceiver, uint256 royaltyAmount) = onDripNFT.royaltyInfo(tokenId, price);
+            (address accountReceiver, uint256 royaltyAmount) = IERC2981(idToMarketItem[itemId].nftContract).royaltyInfo(tokenId, price);
   
             uint256 amountReceived = msg.value;
             uint256 amountAfterRoyalty = amountReceived - royaltyAmount;
@@ -112,14 +157,14 @@ contract OnDripMarket is ReentrancyGuard {
             uint256 amountToSeller = amountAfterRoyalty - amountToMarketplace;
 
             idToMarketItem[itemId].seller.transfer(amountToSeller);
-            payable(address(payoutAddress)).transfer(amountToMarketplace);
+            payable(address(owner)).transfer(amountToMarketplace);
             payable(address(accountReceiver)).transfer(royaltyAmount);
 
-            IERC721(onDripNFT).transferFrom(address(this), msg.sender, tokenId);
+            IERC721(idToMarketItem[itemId].nftContract).transferFrom(address(this), msg.sender, tokenId);
 
             idToMarketItem[itemId].owner = payable(msg.sender);
             _itemsSold.increment();
-            idToMarketItem[itemId].sold = true;
+            idToMarketItem[itemId].forSale = false;
             _itemsSold.increment();
             emit MarketItemSold(itemId, tokenId, msg.sender, price);
 
